1f06654b4d2ba13d48f9b935c8284a30e5271c8e
bhimatak <bhima.t@gmail.com>
2026-07-15 16:17:41 +0530

    updated

---
+++ day01/p4.cpp
@@ -0,0 +1,50 @@
+/*
+Pointers
+
+dt *ptrName;
+
+int *ptr;
+char *name;
+
+char addr[20] = "bangalore";
+
+name = &addr[0];
+
+int a=10;
+double d=20.22;
+int *p = &d;
+
+1. op[] => *op
+2. &* => nullify each other
+    &*op => op
+
+*/
+
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int main()
+{
+    double arr[3] = {10.22,30.3,40.4};
+
+    //double *ptr = &arr[0]; //BA of array
+    double *ptr = arr; //BA of array
+
+
+    printf("\nBA of array = %u\n",&arr[0]);
+    printf("\nBA of array = %u\n",arr);
+    printf("\n1st  element array = %f\n",arr[0]);
+    printf("\n1st  element array = %f\n",*arr);
+    
+    
+
+    cout<<*ptr<<endl;
+    ptr++;
+    cout<<*ptr<<endl;
+    ptr--;
+    cout<<*ptr<<endl;
+    
+
+    return 0;
+}
