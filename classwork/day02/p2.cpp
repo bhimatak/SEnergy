f57dec5e9868046a15bfddd821d6ba560c2a7604
bhimatak <bhima.t@gmail.com>
2026-07-16 10:25:38 +0530

    updated

---
+++ classwork/day02/p2.cpp
@@ -0,0 +1,46 @@
+/*
+
+Pointers
+
+1. null pointer
+2. wild pointer
+3. dangling pointer
+4. void pointer / generic pointer
+
+
+*/
+
+#include <iostream>
+
+using namespace std;
+
+
+int *rValue(int s)
+{
+    static int ret=0; //3000
+    cout<<ret<<endl;
+    ret +=s;
+
+    return &ret;
+}
+
+int main()
+{
+    int *ptr; //wild pointer
+    int *ptr1 = nullptr; // null pointer. int *ptr = NULL;
+
+    //*ptr = 10; //(void  *)0
+
+    void *ptr2=nullptr; //generic ptr
+
+    ptr1 = rValue(10);
+
+    *ptr1 = 101;
+
+    rValue(20);
+
+
+    return 0;
+
+
+}
