6c34e9bc4c0e669255222535125c2c6bc46d9e3e
bhimatak <bhima.t@gmail.com>
2026-07-20 15:26:22 +0530

    updated

--- classwork/day04/p8.cpp
+++ classwork/day04/p8.cpp
@@ -21,7 +21,13 @@ public:
 
         return retObj;
     }
+    friend std::ostream& operator<<(std::ostream& os, rectangle&);
 };
+
+std::ostream& operator<<(std::ostream& os, rectangle& objRect) {
+        os << "Height: " << objRect.h << "\tWidth:  " << objRect.w <<endl;
+        return os;
+    }
 
 // rectangle additionRect(const rectangle &r1 ,const rectangle &r2)
 // {
@@ -31,11 +37,23 @@ public:
 
 int main()
 {
+    try{
     rectangle r1(3,4);
     rectangle r2(4,3);
     rectangle r3 = r1+r2;
-    
+    //throw "Waring";
     r3.printArea();
+
+    // char *name = new char[0];
+    // strcpy(name, "bhima");
+    
+    cout<<r3;
+    }
+    catch(exception &e)
+    {
+        cerr<<e.what()<<endl;
+    }
+    
 
     // cout<<r3;
     // cin>>r3;
