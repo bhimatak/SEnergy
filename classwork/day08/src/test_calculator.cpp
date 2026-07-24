eeaf2d8c5a8b4b57792241ed1aaf29e7e1eeae75
bhimatak <bhima.t@gmail.com>
2026-07-24 11:32:04 +0530

    updated

--- classwork/day08/src/test_calculator.cpp
+++ classwork/day08/src/test_calculator.cpp
@@ -1,9 +1,17 @@
-#include <gtest.h>
+/*
+ g++ ./src/test_calculator.cpp ./src/Calc.cpp -I C:/MinGW/include/ -I ./inc -lgtest -l gtest_main -o test_calcultor.exe
+
+ .\test_calcultor.exe
+
+*/
+
+#include <gtest/gtest.h>
 #include <Calc.h>
 
 TEST(CalculatorTest, AddsTwoPositiveNumbers) {
     Calc calc;
-    EXPECT_EQ(calc.addition(2, 3), 5);
+    ASSERT_EQ(calc.addition(2, 3), 6);
+    std::cout<<"Calc EXPECT is Done"<<std::endl;
 }
 
 TEST(CalculatorTest, SubtractsCorrectly) {
